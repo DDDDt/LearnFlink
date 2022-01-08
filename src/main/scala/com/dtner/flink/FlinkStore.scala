e5b26ab3e154cf18cec1e52de28c8962bf8003d7
混日子的码农 <245548212@qq.com>
2022-01-08 14:27:29 +0800

    修改 flink 版本

---
+++ src/main/scala/com/dtner/flink/FlinkStore.scala
@@ -0,0 +1,22 @@
+package com.dtner.flink
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+
+/**
+ * @program: com.learn.flink
+ * @description: flink 启动类
+ * @author: dt
+ * @create: 2022-01-08
+ * */
+object FlinkStore {
+
+  def main(args: Array[String]): Unit = {
+
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+
+
+
+  }
+
+}
